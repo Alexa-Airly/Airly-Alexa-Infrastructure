e6942d29f5680dd96fd3fb0929530fdf2df40646
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-11-30 20:07:00 +0100

    fix typo

--- provision/main.tf
+++ provision/main.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket" "ArtifactsBucket" {
   }
 }
 
-esource "aws_dynamodb_table" "TfStateTable" {
+resource "aws_dynamodb_table" "TfStateTable" {
   name         = "${var.table_name}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
